61657fbafc9d665faa9706b03a903f5c225f338f
pongpak726 <fuza2560@gmail.com>
2025-12-25 22:06:21 +0700

    Addamount

--- lib/cloth-detail.dart
+++ lib/cloth-detail.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:uniqlo/model/cloth.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class ClothDetail extends StatelessWidget{
- Cloth cloth;
+class ClothDetail extends StatefulWidget{
+ final Cloth cloth;
 
-  ClothDetail({super.key, required this.cloth});
-
+  const ClothDetail({super.key, required this.cloth});
 
   @override
+  State<ClothDetail> createState() => ClothDetailState();
+ 
+}
+
+class ClothDetailState extends State<ClothDetail>{
+  int qty = 1;
+  
+  @override
   Widget build(BuildContext context) {
+   final totalPrice = widget.cloth.imagePrice * qty;
+   
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cloth Detail'),
@@ -17,26 +26,97 @@ class ClothDetail extends StatelessWidget{
       body: Center(
          child: Column(
         children: [
-        Image.asset(cloth.imageUrl),
+        Image.asset(widget.cloth.imageUrl , fit: BoxFit.contain),
         const SizedBox(height: 20.0,),
         Text(
-          cloth.imgLabel,
+          widget.cloth.imgLabel,
           style: const TextStyle(
             fontSize: 24.0,
             fontWeight: FontWeight.bold
           ),
         ),
         Text(
-          cloth.imageText,
+          widget.cloth.imageText,
           style: GoogleFonts.roboto(
             fontSize: 20.0,
             fontWeight:FontWeight.bold
           ),
         ),
+
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+          
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    if (qty > 1) qty--;
+                  });
+                },
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 1),
+                  ),
+                  child: const Center(
+                    child: Text("-", 
+                    style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+              ),
+          
+              const SizedBox(width: 16),
+          
+              Text("$qty", 
+              style: const TextStyle(fontSize: 20)),
+          
+              const SizedBox(width: 16),
+          
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    qty++;
+                  });
+                },
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 1),
+                  ),
+                  child: const Center(
+                    child: Text("+", 
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ),
+
+              Spacer(),
+
+                  GestureDetector(
+                    onTap: () {}, 
+                    child: Center(
+                      child: Text(
+                        "Total ฿${totalPrice.toStringAsFixed(0)}",
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  )
+
+            ],
+          ),
+        ),
+
         ]
          ),
       ),
     );
   }
- 
+
 }
+ 
